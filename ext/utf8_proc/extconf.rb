44e56d7f71aa126e09c2fa8532a2f7b1e1db7228
Tim Bellefleur <nomoon@phoebus.ca>
2017-02-23 13:55:29 -0800

    Add libutf8proc as a submodule and fallback to it if local dylib isn’t found.

--- ext/utf8_proc/extconf.rb
+++ ext/utf8_proc/extconf.rb
@@ -2,10 +2,22 @@
 # rubocop:disable GlobalVars
 require "mkmf"
 
-$CFLAGS << " -std=c99 -Wno-declaration-after-statement"
-
 pkg_config("utf8proc")
+unless have_library("utf8proc")
+  puts "Compiling local libutf8proc..."
 
-have_library("utf8proc") || abort("This extension requires the utf8proc library.")
+  libutf8proc_dir = File.expand_path(File.join(File.dirname(__FILE__),
+                                               "../../vendor/libutf8proc"))
+  Dir.chdir(libutf8proc_dir) do
+    system("make libutf8proc.a")
+  end
+
+  dir_config("utf8_proc/utf8_proc",
+             [RbConfig::CONFIG["includedir"], libutf8proc_dir],
+             [RbConfig::CONFIG["libdir"], libutf8proc_dir])
+  $LOCAL_LIBS << " -lutf8proc"
+end
+
+$CFLAGS << " -std=c99 -Wno-declaration-after-statement"
 
 create_makefile("utf8_proc/utf8_proc")
