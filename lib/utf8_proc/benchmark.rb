9686ff812baa330af36f7e18ca135437549f0e26
Tim Bellefleur <nomoon@phoebus.ca>
2017-02-27 23:09:16 -0800

    Generate a sample set of un-normalized strings for the Benchmarks.

--- lib/utf8_proc/benchmark.rb
+++ lib/utf8_proc/benchmark.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# rubocop:disable CyclomaticComplexity, MethodLength
+# rubocop:disable MethodLength
 module UTF8Proc
   module Benchmark
     module_function
@@ -7,60 +7,59 @@ module UTF8Proc
     def run
       require "benchmark/ips"
       require "unf"
-      test_string = "\u{1D160} \u{FB2C} \u{0390} \u{1F82} \u{FDFA} "
-      test_string *= 10
+      # Various different normalizations of Unicode characters.
+      test_arr = ["\u{03D3}", "\u{03D2 0301}", "\u{038E}", "\u{03A5 0301}",
+                  "\u{03D4}", "\u{03D2 0308}", "\u{03AB}", "\u{03A5 0308}",
+                  "\u{1E9B}", "\u{017F 0307}", "\u{1E61}", "\u{0073 0307}",
+                  "\u{1D160}", "\u{1D158 1D165 1D16E}",
+                  "\u{1F82}", "\u{03B1 0313 0300 0345}",
+                  "\u{FDFA}", "\u{0635 0644 0649 0020 0627 0644 0644 0647}" \
+                  "\u{0020 0639 0644 064A 0647 0020 0648 0633 0644 0645}"] * 10
+      test_arr.concat([" "] * (test_arr.length / 4))
 
-      test_string_nfc = test_string.unicode_normalize(:nfc)
-      test_string_nfd = test_string.unicode_normalize(:nfd)
-      test_string_nfkc = test_string.unicode_normalize(:nfkc)
-      test_string_nfkd = test_string.unicode_normalize(:nfkd)
-
-      puts "Test strings:\n\n" \
-           "NFC:  #{test_string_nfc}\n\n" \
-           "NFD:  #{test_string_nfd}\n\n" \
-           "NFKC:  #{test_string_nfkc}\n\n" \
-           "NFKD:  #{test_string_nfkd}\n\n" \
+      test_strings = Array.new(20) { test_arr.sample(30).join("") }
+      puts "\nBenchmark strings:\n\n * #{test_strings.join("\n * ")}\n\n"
 
       ::Benchmark.ips do |x|
         x.report("UNF NFC") do
-          raise unless UNF::Normalizer.normalize(test_string_nfd, :nfc) == test_string_nfc
+          UNF::Normalizer.normalize(test_strings.sample, :nfc)
         end
 
         x.report("UTF8Proc NFC") do
-          raise unless UTF8Proc.normalize(test_string_nfd, :nfc) == test_string_nfc
+          UTF8Proc.normalize(test_strings.sample, :nfc)
         end
         x.compare!
       end
 
       ::Benchmark.ips do |x|
         x.report("UNF NFD") do
-          raise unless UNF::Normalizer.normalize(test_string_nfc, :nfd) == test_string_nfd
+          UNF::Normalizer.normalize(test_strings.sample, :nfd)
         end
 
         x.report("UTF8Proc NFD") do
-          raise unless UTF8Proc.normalize(test_string_nfc, :nfd) == test_string_nfd
+          UTF8Proc.normalize(test_strings.sample, :nfd)
         end
         x.compare!
       end
 
       ::Benchmark.ips do |x|
         x.report("UNF NFKC") do
-          raise unless UNF::Normalizer.normalize(test_string_nfc, :nfkc) == test_string_nfkc
+          UNF::Normalizer.normalize(test_strings.sample, :nfkc)
         end
 
         x.report("UTF8Proc NFKC") do
-          raise unless UTF8Proc.normalize(test_string_nfc, :nfkc) == test_string_nfkc
+          UTF8Proc.normalize(test_strings.sample, :nfkc)
         end
         x.compare!
       end
 
       ::Benchmark.ips do |x|
         x.report("UNF NFKD") do
-          raise unless UNF::Normalizer.normalize(test_string_nfc, :nfkd) == test_string_nfkd
+          UNF::Normalizer.normalize(test_strings.sample, :nfkd)
         end
 
         x.report("UTF8Proc NFKD") do
-          raise unless UTF8Proc.normalize(test_string_nfc, :nfkd) == test_string_nfkd
+          UTF8Proc.normalize(test_strings.sample, :nfkd)
         end
         x.compare!
       end
