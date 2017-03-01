ef6c632de75a31fe8975db7a7a1f2acb08acdf4f
Tim Bellefleur <nomoon@phoebus.ca>
2017-03-01 14:23:16 -0800

    Tweak benchmark

--- lib/utf8_proc/benchmark.rb
+++ lib/utf8_proc/benchmark.rb
@@ -18,10 +18,10 @@ module UTF8Proc
                   "\u{1D160}", "\u{1D158 1D165 1D16E}",
                   "\u{1F82}", "\u{03B1 0313 0300 0345}",
                   "\u{FDFA}", "\u{0635 0644 0649 0020 0627 0644 0644 0647}" \
-                  "\u{0020 0639 0644 064A 0647 0020 0648 0633 0644 0645}"] * 10
-      test_arr.concat([" "] * (test_arr.length / 4))
+                  "\u{0020 0639 0644 064A 0647 0020 0648 0633 0644 0645}"] * 2
+      test_arr.concat([" "] * (test_arr.length / 3))
 
-      test_strings = Array.new(20) { test_arr.sample(30).join("") }
+      test_strings = Array.new(20) { test_arr.sample(15).join("") }
       puts "\nBenchmark strings:\n\n * #{test_strings.join("\n * ")}\n\n"
 
       ::Benchmark.ips do |x|
@@ -33,17 +33,25 @@ module UTF8Proc
         x.report("UTF8Proc NFC") do
           UTF8Proc.normalize(test_strings.sample, :nfc)
         end
+
+        x.report("Ruby NFC") do
+          test_strings.sample.unicode_normalize(:nfc)
+        end
         x.compare!
       end
 
       ::Benchmark.ips do |x|
-        x.config(time: 10, warmup: 2)
+        x.config(time: 5, warmup: 1)
         x.report("UNF NFD") do
           UNF::Normalizer.normalize(test_strings.sample, :nfd)
         end
 
         x.report("UTF8Proc NFD") do
           UTF8Proc.normalize(test_strings.sample, :nfd)
+        end
+
+        x.report("Ruby NFD") do
+          test_strings.sample.unicode_normalize(:nfd)
         end
         x.compare!
       end
@@ -57,6 +65,10 @@ module UTF8Proc
         x.report("UTF8Proc NFKC") do
           UTF8Proc.normalize(test_strings.sample, :nfkc)
         end
+
+        x.report("Ruby NFKC") do
+          test_strings.sample.unicode_normalize(:nfkc)
+        end
         x.compare!
       end
 
@@ -69,6 +81,10 @@ module UTF8Proc
         x.report("UTF8Proc NFKD") do
           UTF8Proc.normalize(test_strings.sample, :nfkd)
         end
+
+        x.report("Ruby NFKD") do
+          test_strings.sample.unicode_normalize(:nfkd)
+        end
         x.compare!
       end
 
@@ -80,6 +96,10 @@ module UTF8Proc
 
         x.report("UTF8Proc NFKC_CF") do
           UTF8Proc.normalize(test_strings.sample, :nfkc_cf)
+        end
+
+        x.report("Ruby NFKC with .downcase") do
+          test_strings.sample.unicode_normalize(:nfkc).downcase
         end
         x.compare!
       end
